1c0e57fbb964352764862f5738f8f985474a939a
Thomas <thomas.girniak@gmail.com>
2018-06-29 00:27:45 +0300

    Main class for test

---
+++ (6) Facade Pattern/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+
+    public static void main(String[] args) {
+
+        FruitsFacade facade = new FruitsFacade();
+
+        facade.appleInfo();
+        facade.peachInfo();
+        facade.pearInfo();
+    }
+}
